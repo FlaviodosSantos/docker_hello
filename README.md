437cdd03ca92c825c1d1626b84629eec652d98d9
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2022-12-07 17:31:20 -0300

    mod readme e index

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Simples projeto pra iniciar estudos do Docker
+
+seguindo esse video:
+https://www.youtube.com/watch?v=AVNADGzXrrQ
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World teste");
+  res.send("Hello World teste");
 });
 
 app.listen(PORT, HOST);
